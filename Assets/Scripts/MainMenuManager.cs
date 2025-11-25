168ef7518b282d896e422f1a9f9e4ba17a1d51b1
Sleeplessgirl1 <rodriguezjacqueline01@outlook.com>
2025-11-24 23:01:14 -0600

    up

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,22 +1,13 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; // ¡Esta línea es importante para cambiar de escena!
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Esta función se llamará cuando el jugador presione el botón de "Start"
+
     public void StartGame()
     {
-        // Carga la escena que se llama "Tutorial Level"
-        // ¡Asegúrate de que el nombre coincida exactamente!
+
         SceneManager.LoadScene("Tutorial Level");
     }
 
-    // Esta función se llamará cuando el jugador presione el botón de "Quit"
-    public void QuitGame()
-    {
-        // Esto solo cierra el juego cuando está compilado, no en el editor.
-        // En el editor, solo verás este mensaje en la consola.
-        Debug.Log("Saliendo del juego...");
-        Application.Quit();
-    }
 }
